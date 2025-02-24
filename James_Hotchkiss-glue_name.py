f7bcd5e1fe65aefa8d2ad22abaa84b8df71e936c
JamesH11-FML <jhotchkiss11@fingerlakes.edu>
2025-02-24 11:06:23 -0500

    Add files via upload

---
+++ James_Hotchkiss-glue_name.py
@@ -0,0 +1,84 @@
+# FILE NAME - glue_name.py
+
+# NAME: James Hotchkiss   
+# DATE: 02/24/2025
+# BRIEF DESCRIPTION:  
+# Glue function
+
+
+# 1. Make sure you fill out the comments above
+# 2. Write your code in the proper spot
+# 3. Be sure to answer the Reflection Questions below
+# 4. You can see the Sample Output below, too
+
+
+
+
+
+########## ENTER YER CODE BELOW THIS LINE ##########
+first_name = input('Enter your first name:')
+last_name = input('Enter your last name:')
+
+#combined_str = "{} {}" .format(first_name, last_name)
+print('Hi, ',first_name +' '+ last_name)
+#print('Hello, ')(combined_str)
+    
+    
+    
+########### END YER CODE ABOVE THIS LINE ###########
+
+
+
+
+########################################
+#          SAMPLE OUTPUT
+########################################
+
+'''
+Enter first name: Dave
+Enter last name: Ghidiu
+Hi, Dave Ghidiu
+'''
+
+
+
+'''
+Enter first name: bart
+Enter last name: simpson
+Hi, bart simpson
+'''
+
+
+
+'''
+Enter first name: Paige
+Enter last name: LaBarr
+Hi, Paige LaBarr
+'''
+
+
+
+'''
+Enter first name: Lisa
+Enter last name: Simpson, esquire
+Hi, Lisa Simpson, esquire
+'''
+
+
+
+########################################
+#          REFLECTION QUESTIONS
+########################################
+
+'''
+
+1. What is a lesson you learned during this lab? You do not have to restrict
+   your response to Python. It might be a lesson in time management, a lesson
+   using CodeGrade, or something else.
+
+   working full time and taking two classes is rough.
+
+
+
+
+'''
